b83d734b86f6648cb26860ffcc56b8a283981ed5
sireikan <tokyotokyotokyotokyo@gmail.com>
2023-02-06 22:29:29 +0900

    flyway

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,9 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-mysql")
+
     runtimeOnly("mysql:mysql-connector-java")
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
