1cca30374038d2414e9ddece301a73c81779d587
sireikan <tokyotokyotokyotokyo@gmail.com>
2023-02-27 00:09:10 +0900

    add query

--- src/main/resources/db/migration/V1.0.0__init.sql
+++ src/main/resources/db/migration/V1.0.0__init.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE IF NOT EXISTS sample;
 
 CREATE TABLE user (
-                      id BIGINT AUTO_INCREMENT,
-                      name text NOT NULL,
-                      email varchar(120) NOT NULL,
-                      password text NOT NULL,
-                      PRIMARY KEY (id),
-                      UNIQUE KEY (email)
+    id BIGINT AUTO_INCREMENT,
+    name text NOT NULL,
+    email varchar(120) NOT NULL,
+    password text NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (email)
 );
 
 insert into user (id, name, email, password) values
