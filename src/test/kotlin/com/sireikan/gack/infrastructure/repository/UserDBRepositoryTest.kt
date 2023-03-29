0baf9253b5064dc5b81856e5537e6980b4979b45
sireikan <tokyotokyotokyotokyo@gmail.com>
2023-03-30 05:03:11 +0900

    add tests

--- src/test/kotlin/com/sireikan/gack/infrastructure/repository/UserDBRepositoryTest.kt
+++ src/test/kotlin/com/sireikan/gack/infrastructure/repository/UserDBRepositoryTest.kt
@@ -1,5 +1,7 @@
 package com.sireikan.gack.infrastructure.repository
 
+import com.sireikan.gack.domain.model.user.UserId
+import com.sireikan.gack.domain.model.user.UserName
 import com.sireikan.gack.domain.repository.UserOrderKey
 import com.sireikan.gack.domain.repository.UserRepository
 import com.sireikan.gack.infrastructure.entity.User
@@ -22,6 +24,25 @@ class UserDBRepositoryTest {
     }
 
     @Test
+    fun find() {
+        Mockito.`when`(userMapper.find(1L)).thenReturn(null)
+        val actual = userRepository.find(UserId(1L))
+
+        Assertions.assertNull(actual)
+    }
+
+    @Test
+    fun find_exist() {
+        Mockito.`when`(userMapper.find(1L)).thenReturn(
+            User.create(1L, "name", "2023-01-01 00:00:00")
+        )
+        val actual = userRepository.find(UserId(1L)) ?: return
+
+        Assertions.assertSame(1L, actual.id.userId)
+        Assertions.assertSame("name", actual.name.userName)
+    }
+
+    @Test
     fun findAll() {
         Mockito.`when`(userMapper.findAll("id")).thenReturn(emptyList())
         val actual = userRepository.findAll(UserOrderKey.USER_ID)
@@ -39,5 +60,17 @@ class UserDBRepositoryTest {
         Assertions.assertEquals(1, actual.size)
         Assertions.assertSame(1L, actual.get(0).id.userId)
         Assertions.assertSame("name", actual.get(0).name.userName)
+    }
+
+    @Test
+    fun update() {
+        Mockito.doNothing().`when`(userMapper).update(User.create(1L, "name", "2023-01-01 00:00:00"))
+        userRepository.update(com.sireikan.gack.domain.model.user.User(UserId(1L), UserName("name2")))
+    }
+
+    @Test
+    fun delete() {
+        Mockito.doNothing().`when`(userMapper).delete(1L)
+        userRepository.delete(UserId(1L))
     }
 }
